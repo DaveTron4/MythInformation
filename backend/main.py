2bbc252e74b6f9b3b27aa48d10b9af9a85fcdc2b
DaveTron4 <davidtnt970@gmail.com>
2026-02-22 01:06:11 -0500

    Added vercel domain in allowe_origins

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="MythInformation API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://myth-information.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
